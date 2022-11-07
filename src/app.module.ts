003e222719f28f23c959272a0f91c2735031bee6
YevgenChop <yevgenchop@gmail.com>
2022-11-07 13:51:29 +0100

    chore: scaffold the app, install deps

---
+++ src/app.module.ts
@@ -0,0 +1,40 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import configuration from './config/configuration';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => {
+        return {
+          type: 'postgres',
+          username: configService.get('DB_USER'),
+          password: configService.get('DB_PASS'),
+          port: parseInt(configService.get('DB_PORT')),
+          host: configService.get('DB_HOST'),
+          database: configService.get('DB_NAME'),
+          migrations: ['dist/migrations/*{.ts,.js}'],
+          migrationsTableName: 'migrations_typeorm',
+          migrationsRun: configService.get('DB_MIGRATIONS_RUN') === 'true',
+          synchronize: configService.get('DB_SYNCRONIZE') === 'true',
+          entities: ['dist/**/*.entity{.ts,.js}'],
+        };
+      },
+      inject: [ConfigService],
+    }),
+    ConfigModule.forRoot({
+      load: [configuration],
+    }),
+    UserModule,
+    AuthModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
